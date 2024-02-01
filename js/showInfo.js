5600b6027496727f189f1f4d2ea9f5e27b6ed244
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2024-02-01 02:10:47 +0200

    add rating section

---
+++ js/showInfo.js
@@ -0,0 +1,16 @@
+const moreEl = document.querySelector('.more');
+const detailsElements = document.querySelectorAll('.details');
+
+moreEl.addEventListener('click', event => {
+  detailsElements.forEach(detailsEl => {
+    if (detailsEl.style.display === 'none' || detailsEl.style.display === '') {
+      detailsEl.style.display = 'block';
+    } else {
+      detailsEl.style.display = 'none';
+    }
+  });
+
+  moreEl.innerText = moreEl.innerText === 'more' ? 'less' : 'more';
+});
+
+console.log(moreEl);
