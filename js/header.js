ff15e3536f1d47b722fdd5d260d4460865097b3b
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2024-02-02 13:16:09 +0200

    add svg

--- js/header.js
+++ js/header.js
@@ -1,20 +1,3 @@
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   var header = document.getElementById('myHeader');
-
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     header.style.position = 'fixed';
-//     header.style.top = '0';
-//     header.style.width = '100%';
-//     header.style.backgroundColor = '#333';
-//   } else {
-//     header.style.position = 'relative';
-//     header.style.backgroundColor = '#333';
-//   }
-// }
 document.addEventListener('scroll', function () {
   const header = document.getElementById('site-header');
   const scrollPosition = window.scrollY;
